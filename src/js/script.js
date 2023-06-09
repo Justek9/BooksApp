d5d7e535ad28db826f40e35ba7ce592df70bcf45
Justyna Konikiewicz <zagorskaj@wp.pl>
2023-06-09 09:06:30 +0200

    add hidden class to filtered book, need repair

--- src/js/script.js
+++ src/js/script.js
@@ -1,92 +1,94 @@
-'use strict';
+'use strict'
 {
-  const select = {
-    templateOf: {
-      templateBooks: '#template-book',
-    },
-  };
-  const templates = {
-    templateBooks: Handlebars.compile(document.querySelector(select.templateOf.templateBooks).innerHTML),
-  };
+	const select = {
+		templateOf: {
+			templateBooks: '#template-book',
+		},
+	}
+	const templates = {
+		templateBooks: Handlebars.compile(document.querySelector(select.templateOf.templateBooks).innerHTML),
+	}
 
-  class Book {
-    constructor(data) {
-      this.data = data;
-      this.renderOnPage();
-      this.addToFav();
-      this.filter()
-    }
+	class Book {
+		constructor(data) {
+			this.data = data
+			this.renderOnPage()
+			this.addToFav()
+			this.filter()
+		}
 
-    renderOnPage() {
-      const generatedHTML = templates.templateBooks(this.data);
-      this.element = utils.createDOMFromHTML(generatedHTML);
-      // console.log(this.element)
-      this.booksContainer = document.querySelector('.books-list');
-      // console.log(booksContainer);
-      this.booksContainer.appendChild(this.element);
-    }
+		renderOnPage() {
+			const generatedHTML = templates.templateBooks(this.data)
+			this.element = utils.createDOMFromHTML(generatedHTML)
+			// console.log(this.element)
+			this.booksContainer = document.querySelector('.books-list')
+			// console.log(booksContainer);
+			this.booksContainer.appendChild(this.element)
+		}
 
-    addToFav(){
-      let favoriteBooks = [];
-      this.booksContainer.addEventListener('dblclick', function(event){
-        event.preventDefault();
-        const bookId = event.target.closest('a').getAttribute('data-id');
-        if(!favoriteBooks.includes(bookId)){
-          favoriteBooks.push(bookId);
-          event.target.closest('a').classList.add('favorite');}
+		addToFav() {
+			let favoriteBooks = []
+			this.booksContainer.addEventListener('dblclick', function (event) {
+				event.preventDefault()
+				const bookId = event.target.closest('a').getAttribute('data-id')
+				if (!favoriteBooks.includes(bookId)) {
+					favoriteBooks.push(bookId)
+					event.target.closest('a').classList.add('favorite')
+				} else {
+					let index = favoriteBooks.indexOf(bookId)
+					favoriteBooks.splice(index, 1)
+					event.target.closest('a').classList.remove('favorite')
+				}
+			})
+		}
 
-        else {
-          let index = favoriteBooks.indexOf(bookId);
-          favoriteBooks.splice(index, 1);
-          event.target.closest('a').classList.remove('favorite');
-        }
-      });     
-    }
+		filter() {
+			let filtersArray = []
+			this.filters = document.querySelector('.filters')
+			this.filters.addEventListener('change', function (event) {
+				event.preventDefault()
+				let checkboxValue = event.target.value
 
-    filter(){
+				if (!filtersArray.includes(checkboxValue)) {
+					filtersArray.push(checkboxValue)
+				} else {
+					let index = filtersArray.indexOf(checkboxValue)
+					filtersArray.splice(index, 1)
+				}
+				console.log(filtersArray)
+				for (let book of dataSource.books) {
+					//console.log(book)
+					let details = book.details
+					for (let filter of filtersArray) {
+						//console.log(filter)
+						// console.log(Object.keys(details))
+						if (
+							!(
+								(Object.keys(details)[0] === filter && Object.values(details)[0] === true) ||
+								(Object.keys(details)[1] === filter && Object.values(details)[1] === true)
+							)
+						) {
+							const theBook = document.querySelector(`.book > a[data-id="${book.id}"]`)
+							theBook.classList.add('hidden')
+						}
+					}
+				}
+			})
+		}
+	}
 
-      let filters = []
-      this.filters = document.querySelector(".filters")
-      this.filters.addEventListener('change', function(event){
-        event.preventDefault()
-        let checkboxValue = event.target.value
-        if(!filters.includes(checkboxValue)) {
-          filters.push(checkboxValue)
-          // console.log(dataSource.books);
-        } else {
-          let index = filters.indexOf(checkboxValue);
-          filters.splice(index, 1)
-        }
-        })
-        console.log(filters);
+	class ListOfBooks {
+		constructor() {
+			this.data = dataSource.books
+			this.getListofBooks()
+		}
 
-        for (let book of dataSource.books ) {
-          console.log(book.details);
-        
-          for (let filter of filters){
-            console.log(filters);
-        }
-      }
-    }
+		getListofBooks() {
+			for (const book of this.data) {
+				new Book(book)
+			}
+		}
+	}
 
-    
-  }
-
-  class ListOfBooks {
-    constructor() {
-      this.data = dataSource.books;
-      this.getListofBooks();
-    }
-
-    getListofBooks() {
-      for (const book of this.data) {
-        new Book(book);
-      }
-    }
-
-
-  }
-
-  new ListOfBooks();
+	new ListOfBooks()
 }
-
